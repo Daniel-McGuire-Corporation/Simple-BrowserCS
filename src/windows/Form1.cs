730c462eb72ed70aa08b1b64f04b1b22531157e2
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2023-11-23 22:03:30 -0600

    Comments

--- src/windows/Form1.cs
+++ src/windows/Form1.cs
@@ -1,11 +1,10 @@
 // Copyright (C) Daniel McGuire Corporation
+// Simple Browser (v1.0.0)
+// THANKS FOR CONTRIBUTING (or Building from Source)
+// Thanks Andy for your help with this project.
+// 
+// This file is part of Simple Browser. (Obviously)
 //
-// WE ARE VERY CLOSE TO 1.2.0 (just need to add tabs)
-//
-// Simple Browser (v1.0.0) - A very simple browser based on the 
-// Microsoft Edge (Chromium) WebView2 Framework
-// Maybe I can make this using some sort of Chromium Base Framework?
-// THANKS FOR CONTRIBUTING (ignore if building from source lmao)
 // Form1.cs
 using System;
 using System.IO;
@@ -20,11 +19,11 @@ namespace Webview2_Test
     {
         private WebView2 webView;
         private TextBox addressBar;
-
+        
         public Form1()
         {
             InitializeComponent();
-
+            // Line Below sets the title bar text to whatever you want it to be (Simple Web) in this case.
             this.Text = "Simple Web";
 
             Panel toolbar = new Panel()
@@ -34,7 +33,7 @@ namespace Webview2_Test
                 BackColor = ColorTranslator.FromHtml("#F3F3F3"),
             };
             this.Controls.Add(toolbar);
-
+            
             addressBar = new TextBox()
             {
                 Dock = DockStyle.Fill,
@@ -42,7 +41,7 @@ namespace Webview2_Test
             };
 
             toolbar.Controls.Add(addressBar);
-
+            // This event is fired when the user presses Enter in the address bar.
             addressBar.KeyPress += (sender, e) =>
             {
                 if (e.KeyChar == (char)13)
@@ -65,6 +64,7 @@ namespace Webview2_Test
             InitializeAsync();
         }
 
+        // This method is called when the form is loaded.
         private async void InitializeAsync()
         {
             webView = new WebView2
@@ -72,7 +72,6 @@ namespace Webview2_Test
                 Dock = DockStyle.Fill,
             };
 
-            // Set the UserDataFolder property here
             webView.CreationProperties = new CoreWebView2CreationProperties()
             {
                 UserDataFolder = $"C:\\Users\\{Environment.UserName}\\AppData\\Local\\SimpleBrowser"
@@ -85,6 +84,7 @@ namespace Webview2_Test
 
             webView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
 
+            // This event is fired when the user presses the Back button.
             webView.CoreWebView2.SourceChanged += (sender, e) =>
             {
                 string url = webView.CoreWebView2.Source.ToString();
@@ -98,19 +98,19 @@ namespace Webview2_Test
                 }
             };
 
-            // Navigate to the default index.html file
+            // This event is fired when the user presses the Back button.
             string defaultHtmlFilePath = GetDefaultHtmlFilePath();
             webView.CoreWebView2.Navigate(defaultHtmlFilePath);
         }
 
 
-
+        // This method is called when the user navigates to a new page.
         private string GetDefaultHtmlFilePath()
         {
             string filePath = @"C:\Program Files (x86)\SimpleBrowser\Resources\NewTab\NewTab.html";
             return filePath;
         }
-
+        // This method is called when the user navigates to a new page.
         private string GetAppDataHtmlFilePath()
         {
             string filePath = @"C:\Program Files (x86)\SimpleBrowser\Resources\NewTab\NewTab.html";
@@ -119,7 +119,7 @@ namespace Webview2_Test
 
         private void CoreWebView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
-            // Handle navigation completion if needed
+            // No clue what this is for.
         }
     }
 }
