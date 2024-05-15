2e4358ab18178ca7f1b336be03d1ca7f7f8cc2e1
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-05-15 03:11:47 -0500

    Updated Build Source Script
    Might make something similar to compilation utilitys such as Microsofts Razzle, or Ninja
    
    Co-Authored-By: AndrewM1597 <159731293+AndrewM1597@users.noreply.github.com>

--- scripts/windows_build/BuildAll.cmd
+++ scripts/windows_build/BuildAll.cmd
@@ -2,11 +2,20 @@
 echo Setting Title
 title Simple Browser Compiler
 echo Setting Config
+:: Sets NPM to verbose
 set NPM_CONFIG_LOGLEVEL=verbose
+:: Sets path variable
 set Path=%~dp0;C:\Program Files (x86)\NSIS\;%PATH%
 echo Changing Directory
 cd %~dp0
 echo Launching
 cls
 echo C# Non-Universal Compiling Script for Simple Browser
-powershell.exe -ExecutionPolicy Bypass -File BuildAll.ps1
+:: Argument Handler
+if "%~1"=="" (
+    :: If no arguments are provided, just execute the PowerShell script as Release
+    powershell.exe -ExecutionPolicy Bypass -File BuildAll.ps1 release
+) else (
+    :: If arguments are provided, pass them to the PowerShell script
+    powershell.exe -ExecutionPolicy Bypass -File BuildAll.ps1 %*
+)
---
+++ scripts/windows_build/docs/CLI.md
@@ -0,0 +1,9 @@
+Usage:
+  .\BuildAll.cmd [-Type <Type>] [-Verbose]
+
+Options:
+  -Type, -t <Type>      Specifies the build configuration. Accepted values are 'Release' or 'Debug'. 
+  (Defaults to 'Release' if not specified)
+  
+  -Verbose, -v          Indicates whether to display detailed build output
+ 
