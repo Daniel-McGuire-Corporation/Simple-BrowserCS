f41f2c8e86bff32473f54b60b4d953ce1b9ed8ec
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-07-17 08:20:08 -0500

    Added release cycle component

--- computechecksum.cmd
+++ computechecksum.cmd
@@ -1,4 +1,7 @@
 @echo off
+title Renaming Release
+pwsh ./rn.ps1
+cls
 title DMC Setup Checksum Generator (PWSH)
 echo Writing checksum to checksum.txt
 echo.
---
+++ rn.ps1
@@ -0,0 +1,13 @@
+﻿# Get the current date in DD-MM-YY format
+$dateSuffix = Get-Date -Format "dd-MM-yy"
+
+# Get all files matching *Setup.exe in the current directory
+$files = Get-ChildItem -Filter "*Setup.exe"
+
+foreach ($file in $files) {
+    # Construct the new file name with the date suffix
+    $newName = $file.Name -replace "Setup.exe", "Setup-$dateSuffix.exe"
+
+    # Rename the file
+    Rename-Item -Path $file.FullName -NewName $newName
+}
